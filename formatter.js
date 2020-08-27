9b4339a5ff1011fca71127226b1ed532d1af1d12
YaroslavDrozdovskiy <54240910+YaroslavDrozdovskiy@users.noreply.github.com>
2020-08-28 02:01:28 +1000

    Update formatter.js

--- formatter.js
+++ formatter.js
@@ -26,13 +26,13 @@ button_copy.addEventListener("click", function(e) {
 
 button_clear.addEventListener("click", function(e) {
   textarea.select();
-//   try {
-//     document.execCommand("copy");
-//   } catch (err) {
-//     alert("Ошибка при копировании. Повторите попытку");
-//     return "Error"
-//   }
-  //   очистим выделение текста, чтобы пользователь "не парился"
+  try {
+    document.execCommand("copy");
+  } catch (err) {
+    alert("Ошибка при копировании. Повторите попытку");
+    return "Error"
+  }
+    очистим выделение текста, чтобы пользователь "не парился"
   document.getSelection().removeAllRanges();
   textarea.value = "";
 });
